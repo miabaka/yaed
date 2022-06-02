75110bc3999ece073f4250daa57ab9256fecb68b
nullium21 <dev@lina.moe>
2022-06-02 14:11:22 +0300

    implement a not really working S:TH exporter

--- editor/src/exporter/TreasureHunterWorldExporter.cpp
+++ editor/src/exporter/TreasureHunterWorldExporter.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "TreasureHunterWorldExporter.hpp"
 
 namespace fs = std::filesystem;
@@ -12,5 +13,28 @@ std::set<std::string> TreasureHunterWorldExporter::supportedGamesIds() const {
 }
 
 void TreasureHunterWorldExporter::save(const World &world, const std::filesystem::path &path) const {
+  std::ofstream file(path, std::ios::binary);
 
+  char header[12] = { 'T', 'H', 0 };
+  *((uint32_t *) &header[8]) = world.levels().size();
+  file.write(header, sizeof(header));
+
+  char level_header[20] = { 0 };
+  for (const auto &level : world.levels()) {
+    *((uint16_t *) &level_header[0]) = level->skinIndex();
+
+    glm::ivec2 size = level->layers()[0]->tilemap().size();
+    *((uint16_t *) &level_header[16]) = size.x;
+    *((uint16_t *) &level_header[18]) = size.y;
+    file.write(level_header, sizeof(level_header));
+
+    static_assert(std::is_same<Tilemap::tile_t, uint16_t>());
+
+    for (const auto &layer : level->layers()) {
+      const auto &tilemap = layer->tilemap();
+      file.write(reinterpret_cast<const char *>(tilemap.data()), sizeof(Tilemap::tile_t) * tilemap.tileCount());
+    }
+  }
+
+  file.flush();
 }
