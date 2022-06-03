19d42031946af3ba3181feb695464e1dc7206dd3
nullium21 <dev@lina.moe>
2022-06-03 20:29:40 +0300

    Add some basic X11 code

--- cutegl/src/X11Platform.cpp
+++ cutegl/src/X11Platform.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "X11Platform.hpp"
+
+using namespace CuteGL::X11;
+
+X11Platform::X11Platform() {
+    dpy = ::X11::XOpenDisplay(nullptr);
+}
+
+X11Platform::~X11Platform() {
+    ::X11::XCloseDisplay(dpy);
+}
--- cutegl/src/X11Platform.hpp
+++ cutegl/src/X11Platform.hpp
@@ -6,8 +6,16 @@
 
 #include "IPlatform.hpp"
 
+extern "C" namespace X11 {
+#include "X11/Xlib.h"
+}
+
 namespace CuteGL::X11 {
     class X11Platform : public IPlatform {
+        ::X11::Display *dpy;
 
+    public:
+        X11Platform();
+        ~X11Platform() override;
     };
 }
